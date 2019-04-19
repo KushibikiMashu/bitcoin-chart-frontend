a9a06a3c4e9ec0e0d89f22231bf48e6faa2f0f35
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-19 22:28:45 +0900

    コードをフォーマット

--- js/multi.js
+++ js/multi.js
@@ -2,9 +2,8 @@
 // ajaxのjson取得アドレスをサーバーに応じて変更する
 
 var seriesOptions = [],
-    seriesCounter = 0,
-    names = ['Zaif', 'bitflyer', 'coincheck'];
-
+  seriesCounter = 0,
+  names = ['Zaif', 'bitflyer', 'coincheck'];
 
 /**
  * Create the chart when all data is loaded
@@ -12,73 +11,66 @@ var seriesOptions = [],
  */
 function createChart() {
 
-    Highcharts.stockChart('container', {
+  Highcharts.stockChart('container', {
 
-      chart: {
-        height: 525,
+    chart: {
+      height: 525,
+    },
+    title: {
+      text: 'Bitcoin price of main exchanges'
+    },
+    rangeSelector: {
+      allButtonsEnabled: false,
+      selected: 0,
+      buttons: [{
+        type: 'minute', // 分単位 (0)
+        count: 1440,     // 単位は分
+        text: '1d'       // 一日分
+      }, {
+        type: 'minute',    // 日単位 (1)
+        count: 1440 * 7,   // 1週間分
+        text: '7d'
+      }, {
+        type: 'minute',    // 日単位 (2)
+        count: 1440 * 7 * 2,   // 2週間分
+        text: '2w'
+      }, {
+        type: 'all',    // 全データ(3)
+        count: 1,
+        text: 'All'
+      }]
+    },
+    yAxis: {
+      plotLines: [{
+        value: 0,
+        width: 2,
+        color: 'white',
+      }],
+      opposite: false,
+    },
+    legend: {
+      enabled: true
+    },
+    plotOptions: {
+      series: {
+        compare: 'price',
+        showInNavigator: true
       },
-
-      title: {
-        text: 'Bitcoin price of main exchanges'
-      },
-
-        rangeSelector: {
-            allButtonsEnabled: false,
-            selected: 0,
-            buttons : [{
-                type : 'minute', // 分単位 (0)
-                count : 1440,     // 単位は分
-                text : '1d'       // 一日分
-            }, {
-                type : 'minute',    // 日単位 (1)
-                count : 1440 * 7,   // 1週間分
-                text : '7d'
-            }, {
-                type : 'minute',    // 日単位 (2)
-                count : 1440 * 7 * 2,   // 2週間分
-                text : '2w'
-            },  {
-                type : 'all',    // 全データ(3)
-                count : 1,
-                text : 'All'
-            }]
-        },
-
-        yAxis: {
-            plotLines: [{
-                value: 0,
-                width: 2,
-                color: 'white',
-            }],
-	    opposite:false,
-        },
-
-      legend: {
-        enabled: true
-      },
-
-        plotOptions: {
-            series: {
-                compare: 'price',
-                showInNavigator: true
-            },
-        },
-
-        tooltip: {
-            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y:,1f}</b><br/>',
-            valueDecimals: 0,
-            split: true,
-        },
-
-        series: seriesOptions
-    });
+    },
+    tooltip: {
+      pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y:,1f}</b><br/>',
+      valueDecimals: 0,
+      split: true,
+    },
+    series: seriesOptions
+  });
 }
 
 // カンマ区切り
 Highcharts.setOptions({
-    global: {
-        useUTC: false
-    },
+  global: {
+    useUTC: false
+  },
   lang: {
     // 桁区切りの文字を指定
     thousandsSep: ',',
@@ -87,20 +79,18 @@ Highcharts.setOptions({
 });
 
 $.each(names, function (i, name) {
+  $.getJSON('http://bitcoin-chart.info/js/json/' + name.toLowerCase() + '.json', function (data) {
+    seriesOptions[i] = {
+      name: name,
+      data: data
+    };
 
-    $.getJSON('http://bitcoin-chart.info/js/json/' + name.toLowerCase() + '.json',    function (data) {
+    // As we're loading the data asynchronously, we don't know what order it will arrive. So
+    // we keep a counter and create the chart when all the data is loaded.
+    seriesCounter += 1;
 
-        seriesOptions[i] = {
-            name: name,
-            data: data
-        };
-
-        // As we're loading the data asynchronously, we don't know what order it will arrive. So
-        // we keep a counter and create the chart when all the data is loaded.
-        seriesCounter += 1;
-
-        if (seriesCounter === names.length) {
-            createChart();
-        }
-    });
+    if (seriesCounter === names.length) {
+      createChart();
+    }
+  });
 });
