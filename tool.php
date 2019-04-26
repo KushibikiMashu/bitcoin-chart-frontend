dcf918dafac771247872f0b1894d52ad0dcae9a2
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-27 01:21:42 +0900

    クラスを作成

--- tool.php
+++ tool.php
@@ -1,64 +1,84 @@
 <?php
 
-$data = [];
-
-$fp = fopen('data/bitcoin_exchange.csv', 'r');
-while (($csv = fgetcsv($fp)) !== false) {
-    $data[] = [
-        'id'       => $csv[0],
-        'name'     => $csv[1],
-        'buy'      => $csv[2],
-        'datetime' => $csv[3],
-    ];
+interface CsvProcessor
+{
+    public function run(): void;
 }
-fclose($fp);
 
-$zaif      = [];
-$bitflyer  = [];
-$coincheck = [];
+class BitcoinChartCsvProcessor implements CsvProcessor
+{
+    public function run(): void
+    {
+        $data = self::read('bitcoin_exchange');
+        [$zaif, $bitflyer, $coincheck] = self::split($data);
+        self::write([$zaif, $bitflyer, $coincheck]);
+    }
 
-$i = $j = $k = 1;
+    private static function read(string $filename): array
+    {
+        $data = [];
+        $fp   = fopen("data/{$filename}.csv", 'r');
+        while (($csv = fgetcsv($fp)) !== false) {
+            $data[] = [
+                'id'       => $csv[0],
+                'name'     => $csv[1],
+                'buy'      => $csv[2],
+                'datetime' => $csv[3],
+            ];
+        }
+        fclose($fp);
+        return $data;
+    }
 
-foreach ($data as $key => $row) {
-    switch ($key) {
-        case($key % 3 === 0):
-            $zaif[] = $row;
-            unset($zaif[$i - 1]['name']);
-            $zaif[$i - 1]['id'] = $i;
-            $i++;
-            break;
-        case($key % 3 === 1):
-            $bitflyer[] = $row;
-            unset($bitflyer[$i - 1]['name']);
-            $bitflyer[$j - 1]['id'] = $j;
-            $j++;
-            break;
-        case($key % 3 === 2):
-            $coincheck[] = $row;
-            unset($coincheck[$i - 1]['name']);
-            $coincheck[$k - 1]['id'] = $k;
-            $k++;
-            break;
-        default:
-            break;
+    private static function split(array $data): array
+    {
+        $zaif = $bitflyer = $coincheck = [];
+        $i    = $j = $k = 0;
+
+        foreach ($data as $key => $row) {
+            switch ($key) {
+                case($key % 3 === 0):
+                    $zaif[] = $row;
+                    unset($zaif[$i]['name']);
+                    $zaif[$i]['id'] = $i+1;
+                    $i++;
+                    break;
+                case($key % 3 === 1):
+                    $bitflyer[] = $row;
+                    unset($bitflyer[$i]['name']);
+                    $bitflyer[$j]['id'] = $j+1;
+                    $j++;
+                    break;
+                case($key % 3 === 2):
+                    $coincheck[] = $row;
+                    unset($coincheck[$i]['name']);
+                    $coincheck[$k]['id'] = $k+1;
+                    $k++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return [$zaif, $bitflyer, $coincheck];
+    }
+
+    private static function write(array $targets): void
+    {
+        $files = [
+            'zaif'      => $targets[0],
+            'bitflyer'  => $targets[1],
+            'coincheck' => $targets[2],
+        ];
+
+        foreach ($files as $filename => $exchange) {
+            $f = fopen("data/{$filename}.csv", 'w');
+            foreach ($exchange as $line) {
+                fputcsv($f, $line);
+            }
+            fclose($f);
+        }
     }
 }
 
-//var_dump($zaif);
-
-$files = [
-    'zaif' => $zaif,
-    'bitflyer' => $bitflyer,
-    'coincheck' => $coincheck,
-];
-
-foreach ($files as $filename => $exchange) {
-    $f = fopen("data/{$filename}.csv", 'w');
-    foreach ($exchange as $line) {
-        fputcsv($f, $line);
-    }
-    fclose($f);
-}
-
-//file_put_contents('./zaif.csv', $zaif);
-
+(new BitcoinChartCsvProcessor())->run();
