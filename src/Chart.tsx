330d90b4be6551008d7856b3c4b0943f2d42eee4
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-29 20:10:34 +0900

    微修正

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -3,13 +3,13 @@ import StockChart from './StockChart'
 
 const Chart: React.FC = () => (
   <div className="text-center">
-    <div className="container pt-5 pb-3">
+    <div className="chart my-5">
+      <StockChart />
+    </div>
+    <div className="container pt-5">
       <h1 className="text-primary h3">ビットコインチャート</h1>
       <p>このチャートでは、ビットコインの15分ごとの金額を各取引所ごとに比較しています。</p>
       <p>価格は15分毎の購入価格です。チャートは15分ごとに更新されます。</p>
-      <div className="chart">
-        <StockChart />
-      </div>
     </div>
   </div>
 )
